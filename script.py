0f12043652c3fcfaf6f2ab6da96b3fb47fbd75d0
Justin Widney <jwidney@cs.ualberta.ca>
2019-01-10 15:01:49 -0700

    name changed added .py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import requests
 
 try:
     
-    request = requests.get("https://raw.githubusercontent.com/justinwidney/404Lab1/master/script")
+    request = requests.get("https://raw.githubusercontent.com/justinwidney/404Lab1/master/script.py")
     open('script.py', 'wb').write(request.content)
     
 
