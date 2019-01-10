bfa5d652d02183e0a12841de0bc1cced3af94479
Justin Widney <jwidney@cs.ualberta.ca>
2019-01-10 15:00:36 -0700

    Script now downloads itself from github

---
+++ script.py
@@ -0,0 +1,19 @@
+import requests
+
+
+try:
+    
+    request = requests.get("https://raw.githubusercontent.com/justinwidney/404Lab1/master/script")
+    open('script.py', 'wb').write(request.content)
+    
+
+    print(request.text)
+    
+except ConnectionError:
+    
+    print("error")
+
+
+
+
+
